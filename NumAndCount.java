78172c8a8495fac142841c5caa61920fcdbf7bc5
oz-ben-zaken <59560826+oz-ben-zaken@users.noreply.github.com>
2022-03-01 18:32:56 +0200

    Create NumAndCount.java

---
+++ NumAndCount.java
@@ -0,0 +1,19 @@
+public class NumAndCount {
+    int num;
+    int count;
+
+
+    public NumAndCount() {}
+    public NumAndCount(int num, int count) {
+        this.num = num;
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "NumAndCount{" +
+                "num=" + num +
+                ", count=" + count +
+                '}';
+    }
+}
